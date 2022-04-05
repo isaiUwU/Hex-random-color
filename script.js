810eef72985c2f70f5288bf38fdefc5733cfffa3
isaiUwU <isaiwalle@gmail.com>
2022-04-05 16:41:36 -0500

    fix the hex code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changeColor(){
-    var hexNumbers = ["0","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    var hexNumbers = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 
     var hexCode = '';
 
